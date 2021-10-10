c10611237331b69d3ea89d1495981a75af5d615b
Kien Le <tonykien2012@gmail.com>
2021-10-10 14:22:05 +0700

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -6,7 +6,7 @@ echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-
 apt-get install -y iptables-persistent
 ln -s /etc/init.d/netfilter-persistent /etc/init.d/iptables
 cd /etc/iptables
-cp $PWD/rules.v4 rules.v4
-cp -p $PWD/rc.local /etc/rc.local
+cp /root/iptables-debian/rules.v4 rules.v4
+cp -p /root/iptables-debian/rc.local /etc/rc.local
 systemctl start netfilter-persistent
 systemctl status netfilter-persistent
